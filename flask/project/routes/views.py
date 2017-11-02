de052ed4656bd9e43faa5508c584b3f713aaebea
izconcept <izkevin52@gmail.com>
2017-11-02 09:41:22 -0400

    added route error handling + improved templating

--- flask/project/routes/views.py
+++ flask/project/routes/views.py
@@ -6,3 +6,13 @@ website_blueprint = Blueprint('website_blueprint', __name__)
 @website_blueprint.route('/')
 def index():
     return render_template('index.html')
+
+
+@website_blueprint.errorhandler(404)
+def not_found_error(error):
+    return render_template('404.html', error=error), 404
+
+
+@website_blueprint.errorhandler(500)
+def internal_error(error):
+    return render_template('500.html', error=error), 500
