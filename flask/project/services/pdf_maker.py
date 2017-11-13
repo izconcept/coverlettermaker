32daa4c4b6a102defb2de14de92b94341deed5df
izconcept <izkevin52@gmail.com>
2017-11-13 17:27:24 -0500

    styling pdf output, displaying li

--- flask/project/services/pdf_maker.py
+++ flask/project/services/pdf_maker.py
@@ -1,14 +1,27 @@
 from reportlab.pdfgen import canvas
+from reportlab.platypus import SimpleDocTemplate, Paragraph, Spacer
+from reportlab.lib.styles import getSampleStyleSheet
+from reportlab.lib.units import inch
+
 from io import BytesIO
 
 
 def gen_pdf(items):
     output = BytesIO()
-    p = canvas.Canvas(output)
+
+    doc = SimpleDocTemplate(output)
+    styles = getSampleStyleSheet()
+    story = [Spacer(1, 2 * inch)]
+    style = styles["Normal"]
+
     for item in items:
-        p.drawString(100, 100, item)
-    p.showPage()
-    p.save()
+        p = Paragraph(item, style)
+        story.append(p)
+        story.append(Spacer(1, 0.2 * inch))
+
+    doc.build(story)
+    # doc.showPage()
+    # doc.save()
 
     pdf_out = output.getvalue()
     output.close()
