fe59812a514fba2308b19af0d49ee4fecdee66eb
izconcept <izkevin52@gmail.com>
2017-12-18 21:15:47 -0500

    autoreload + parser update

--- flask/project/services/parser.py
+++ flask/project/services/parser.py
@@ -1,8 +1,18 @@
 from bs4 import BeautifulSoup
+import re
 
 
 def html_parser(html):
     soup = BeautifulSoup(html, 'html.parser')
+
+    requirements = soup.body.findAll(text=re.compile('Skills'))
+
+    print(requirements)
+    print(requirements[0])
+    # if len(requirements) > 0:
+    #     for elem in requirements:
+    #         ul = elem.parent.parent.find_next_sibling('ul')
+    #         print(ul, flush=True)
 
     items = []
 
