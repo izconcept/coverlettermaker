81e9fc5a99e54116d98496ca6a4dc0205e22e632
izconcept <izkevin52@gmail.com>
2018-01-14 17:07:11 -0500

    get calls to query user data

--- flask/project/routes/views.py
+++ flask/project/routes/views.py
@@ -1,4 +1,4 @@
-from flask import render_template, Blueprint, session, request
+from flask import render_template, Blueprint, session, request, redirect
 import requests
 import json
 
@@ -7,6 +7,9 @@ website_blueprint = Blueprint('website_blueprint', __name__)
 
 @website_blueprint.route('/')
 def index():
+    tags = []
+    if 'tags' in session:
+        tags = session['tags']
     if 'code' in request.args:
         code = request.args['code']
         r = requests.post("https://www.linkedin.com/oauth/v2/accessToken", data={'grant_type': 'authorization_code',
@@ -14,12 +17,15 @@ def index():
                                                                                  'redirect_uri': 'http://localhost/',
                                                                                  'client_id': '78rpczh5d7fm7p',
                                                                                  'client_secret': 'rhWRnE3BnmN0Ou3l'})
-
         session['token'] = json.loads(r.text)["access_token"]
-    tags = []
-    if 'tags' in session:
-        tags = session['tags']
-    return render_template('index.html', tags=tags)
+        r = requests.get("https://api.linkedin.com/v1/people/~?format=json", headers={'Authorization': "Bearer " + session['token']})
+        profile = json.loads(r.text)
+        session['user_name'] = profile['firstName'] + " " + profile['lastName']
+
+
+        return render_template('index.html', tags=tags, name=session['user_name'])
+    else:
+        return redirect('/login')
 
 
 @website_blueprint.route('/login')
